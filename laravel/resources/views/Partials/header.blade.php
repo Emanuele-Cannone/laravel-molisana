093d0f5f728d75b46d7bdb25b8b3896776b7d9a2
Emanuele <cannone.emanuele@gmail.com>
2021-03-10 16:04:33 +0100

    collegamento route ok

--- laravel/resources/views/Partials/header.blade.php
+++ laravel/resources/views/Partials/header.blade.php
@@ -1,1 +1,7 @@
 <h3>questo è l'header</h3>
+
+<ul>
+    <li><a href="{{ route('home') }}">home</a></li>
+    <li><a href="{{ route('prodotti') }}">prodotti</a></li>
+    <li><a href="{{ route('novita') }}">novità</a></li>
+</ul>
--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -2,9 +2,5 @@
 
 @section('content')
     <p>io sono home</p>
-    <ul>
-        <li><a href="{{ route('Home') }}">home</a></li>
-        <li><a href="">prodotti</a></li>
-        <li><a href="">novità</a></li>
-    </ul>
+
 @endsection
