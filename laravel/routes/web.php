38c136c9cbf9224ae79c1ec2e651001033f74a4b
Emanuele <cannone.emanuele@gmail.com>
2021-03-10 14:14:53 +0100

    creazione pagine

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/prodotti', function () {
+    return view('product');
+});
+
+Route::get('/novita', function () {
+    return view('news');
 });
