cff70d12c2c27788134ce6bc5df50c9c312b7fa7
Emanuele <cannone.emanuele@gmail.com>
2021-03-11 17:09:03 +0100

    visualizzazione prodotto

--- laravel/resources/views/specific-product.blade.php
+++ laravel/resources/views/specific-product.blade.php
@@ -2,7 +2,10 @@
 
 @section('content')
  
-@dump($selezione)
-
+<div class="description-products">
+    <h1>{{ $nomeAssociativo['titolo'] }}</h1>
+    <img src="{{ $nomeAssociativo['src-h'] }}" alt="">
+    <img src="{{ $nomeAssociativo['src-p'] }}" alt="">
+</div>
 
 @endsection
